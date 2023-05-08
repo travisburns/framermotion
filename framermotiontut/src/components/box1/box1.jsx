178fc2313ba8bdb792ae8a465ebf9f8a5e347e55
Travis <travisjb0@gmail.com>
2023-05-07 17:14:18 -0700

    Added basic animation on click for box1, got a good grasp on animations in framerJS

--- framermotiontut/src/components/box1/box1.jsx
+++ framermotiontut/src/components/box1/box1.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from "framer-motion"
 
 
 export default function box1() {
+
+    const [isAnimating, setIsAnimating] = useState(false)
+
   return (
     <div className='box-container'>
       <motion.div 
@@ -10,12 +13,22 @@ export default function box1() {
       className='box'
       
       animate={{
-         x: 750,
-         opacity: 1
+         x: isAnimating ? 750 : 0, 
+         opacity: isAnimating ? 1 : 0.5,
+         rotate: isAnimating ? 360 : 0,
+         backgroundColor: isAnimating ? "blue" : "black" 
+         
       }}
       initial={{
         opacity: 0.1
       }}
+      transition={{
+        type: 'spring',
+        stiffness: 100,
+        damping: 5
+      }}
+
+      onClick={() => setIsAnimating(!isAnimating)}
       >
 
       </motion.div>
