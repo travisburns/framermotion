1318085ae6614f60198e4cfdff114ea653509e11
Travis <travisjb0@gmail.com>
2023-05-07 19:36:10 -0700

    added box3 stats with variant properties

--- framermotiontut/src/App.jsx
+++ framermotiontut/src/App.jsx
@@ -3,6 +3,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Box1 from './components/Box1/Box1'
 import Box2 from './components/Box2/Box2'
+import Box3 from './components/Box3/Box3'
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
    <div className='App'>
     <Box1 />
     <Box2 />
-
+    <Box3 />
    </div>
       
         
