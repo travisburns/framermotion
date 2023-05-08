463c23e6dee2c8a227f036de36d0557a1eb754ff
Travis <travisjb0@gmail.com>
2023-05-07 19:49:00 -0700

    Added and worked with keyframes on box4.jsx

--- framermotiontut/src/App.jsx
+++ framermotiontut/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Box1 from './components/Box1/Box1'
 import Box2 from './components/Box2/Box2'
 import Box3 from './components/Box3/Box3'
-
+import Box4 from './components/Box4/Box4'
 
 function App() {
   
@@ -14,6 +14,7 @@ function App() {
     <Box1 />
     <Box2 />
     <Box3 />
+    <Box4 />
    </div>
       
         
---
+++ framermotiontut/src/components/Box4/Box4.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { motion } from "framer-motion"
+
+
+export default function box4() {
+
+    
+
+  return (
+    <div className='box-container'>
+      <motion.div 
+      className='box'
+      animate={{
+        scale: [1, 1.4, 1.4, 1, 1, 1], 
+        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+        rotate: [0, 0, 270, 270, 0]
+      }}
+      transition ={{
+        duration: 2
+      }}
+      
+      
+      >
+
+      </motion.div>
+    </div>
+  )
+}
